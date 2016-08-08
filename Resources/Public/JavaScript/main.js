f61dab4e7a31d1fb0765eaee5be15b6c497d0f01
Eugène Fritz <megafry@MBP-de-Eugene.fritz.box>
2016-08-08 22:26:32 +0200

    added Slick.js and Slick.less

---
+++ Resources/Public/JavaScript/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){	
+
+	$('.single-item').slick({
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		autoplay: true,
+		slidesToShow: 1,
+		autoplaySpeed: 2000,
+		adaptiveHeight: false
+
+	});
+
+
+});
+
